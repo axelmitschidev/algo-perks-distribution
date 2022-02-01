0a8f85b3ca1c3e96bd75a27240d05a33ba0531ba
Axel Mitschi <axel.mitschi@gmail.com>
2022-02-01 16:34:30 +0100

    beautifulification

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
-const points = document.getElementById('point')
-const attack = document.getElementById('attack')
-const defence = document.getElementById('defence')
-const life = document.getElementById('life')
+const perks_element = document.getElementById('perks-input');
+const attack_element = document.getElementById('attack-input');
+const defence_element = document.getElementById('defence-input');
+const life_element = document.getElementById('life-input');
 
-const total_points = 100;
+const total_points = 10;
 
-points.innerText = total_points;
-attack.setAttribute('max', total_points)
-defence.setAttribute('max', total_points)
-life.setAttribute('max', total_points)
+perks_element.innerText = total_points;
+
+attack_element.setAttribute('max', total_points);
+defence_element.setAttribute('max', total_points);
+life_element.setAttribute('max', total_points);
 
 let remaining_points = 0;
 
-attack.addEventListener('change', e => {
+attack_element.addEventListener('change', e => {
 	if (remaining_points <= total_points) {
-		remaining_points = total_points - attack.value - defence.value - life.value;
-		defence.setAttribute('max', total_points - attack.value - life.value)
-		life.setAttribute('max', total_points - attack.value - defence.value)
-		points.innerText = remaining_points;
+		remaining_points = total_points - attack_element.value - defence_element.value - life_element.value;
+		defence_element.setAttribute('max', total_points - attack_element.value - life_element.value);
+		life_element.setAttribute('max', total_points - attack_element.value - defence_element.value);
+		perks_element.innerText = remaining_points;
 	}
-})
+});
 
-defence.addEventListener('change',  e => {
+defence_element.addEventListener('change', e => {
 	if (remaining_points <= total_points) {
-		remaining_points = total_points - attack.value - defence.value - life.value;
-		attack.setAttribute('max',  total_points - defence.value - life.value)
-		life.setAttribute('max',  total_points - attack.value - defence.value)
-		points.innerText = remaining_points;
+		remaining_points = total_points - attack_element.value - defence_element.value - life_element.value;
+		attack_element.setAttribute('max',  total_points - defence_element.value - life_element.value)
+		life_element.setAttribute('max',  total_points - attack_element.value - defence_element.value)
+		perks_element.innerText = remaining_points;
 	}
-})
+});
 
-life.addEventListener('change',  e => {
+life_element.addEventListener('change', e => {
 	if (remaining_points <= total_points) {
-		remaining_points = total_points - attack.value - defence.value - life.value;
-		attack.setAttribute('max', total_points - life.value - defence.value)
-		defence.setAttribute('max', total_points - attack.value - life.value)
-		points.innerText = remaining_points;
+		remaining_points = total_points - attack_element.value - defence_element.value - life_element.value;
+		attack_element.setAttribute('max', total_points - life_element.value - defence_element.value)
+		defence_element.setAttribute('max', total_points - attack_element.value - life_element.value)
+		perks_element.innerText = remaining_points;
 	}
-})
-
-/* copyright Axel MITSCHI */
-
+});
